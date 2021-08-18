688ab71481b6dbb789d15a4932444109aaec514d
Jean-Loup Peres <doc.jprs@gmail.com>
2021-08-18 21:46:53 +0200

    peauffinage layout calendrier responsive, + section notice done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'map', to: 'pages#map'
   get 'ship', to: 'pages#ship'
   get 'treasury', to: 'pages#treasury'
+  get 'notices', to: 'pages#notices'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
