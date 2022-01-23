e4711acf69ba96efb4765bcab47b1c2b2bc86368
Jean-Loup Peres <doc.jprs@gmail.com>
2022-01-23 01:48:22 +0100

    remove assets

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module WebAppIcarus
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
